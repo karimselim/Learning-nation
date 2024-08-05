4a3024f22dbeffdaebb9a04fc63b2e1cdc007a33
Karim_Selim <kareemsleem7@gmail.com>
2024-08-05 04:09:50 +0300

    adding loading page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 // import Environment from "./components/Environment";
+import { Loader } from "@react-three/drei";
 import EnvironmentComp from "./components/Environment";
 import "./index.css";
+import { Suspense } from "react";
 
 const App = () => {
-  return <EnvironmentComp />;
+  return (
+    <>
+      <Suspense>
+        <EnvironmentComp />
+      </Suspense>
+      <Loader />
+    </>
+  );
 };
 
 export default App;
--- src/components/Environment.jsx
+++ src/components/Environment.jsx
@@ -1,17 +1,19 @@
 /* eslint-disable react/no-unknown-property */
-import { Gltf, OrbitControls, Environment } from "@react-three/drei";
+import { OrbitControls, Environment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { School } from "./School";
 import { Karim } from "./Karim";
+import { CameraManager } from "./CameraManager";
 
 const EnvironmentComp = () => {
   return (
     <Canvas>
+      <CameraManager />
       <OrbitControls />
       <ambientLight intensity={0.8} color="white" />
       <Environment preset="city" color="white" />
       <School position={[0, -8, 0]} rotation={[0, Math.PI, 0]} />
-      <Karim position={[-11, -7.95, -14]} scale={7} rotation={[0, 0.5, 0]} />
+      <Karim position={[-12, -7.95, -14]} scale={7} rotation={[0, 0.5, 0]} />
     </Canvas>
   );
 };
